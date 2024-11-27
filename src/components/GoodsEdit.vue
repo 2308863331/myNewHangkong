790d9fc7d42206d6778e49f0a6e5209679c2c455
李倩怡 <13454056+li-qi-Anyi@user.noreply.gitee.com>
2024-11-27 22:18:51 +0800

    修改航班回显实现

--- src/components/GoodsEdit.vue
+++ src/components/GoodsEdit.vue
@@ -1,59 +1,80 @@
 <template>
-  <el-form ref="flightFormRef" :model="form" label-width="120px">
-    <!-- 航班号 -->
-    <el-form-item label="航班号" prop="flightNumber" style="width: 92%">
-      <el-input v-model="form.flightNumber" placeholder="请填写航班号" />
-    </el-form-item>
-    <!-- 出发机场 -->
-    <el-form-item label="出发机场" prop="departureAirport" style="width: 92%">
-      <el-input v-model="form.departureAirport" placeholder="请填写出发机场" />
-    </el-form-item>
-    <!-- 到达机场 -->
-    <el-form-item label="到达机场" prop="arrivalAirport" style="width: 92%">
-      <el-input v-model="form.arrivalAirport" placeholder="请填写到达机场" />
-    </el-form-item>
-    <!-- 出发时间 -->
-    <el-form-item label="出发时间" prop="departureTime" style="width: 92%">
-      <el-date-picker
-        v-model="form.departureTime"
-        type="datetime"
-        placeholder="请选择出发时间"
-        format="YYYY-MM-DD HH:mm:ss"
-        value-format="YYYY-MM-DD HH:mm:ss"
-      />
-    </el-form-item>
-    <!-- 到达时间 -->
-    <el-form-item label="到达时间" prop="arrivalTime" style="width: 92%">
-      <el-date-picker
-        v-model="form.arrivalTime"
-        type="datetime"
-        placeholder="请选择到达时间"
-        format="YYYY-MM-DD HH:mm:ss"
-        value-format="YYYY-MM-DD HH:mm:ss"
-      />
-    </el-form-item>
-    <!-- 航班状态 -->
-    <el-form-item label="航班状态" prop="status" style="width: 92%">
-      <el-select v-model="form.status" placeholder="请选择航班状态">
-        <el-option label="按时" value="按时" />
-        <el-option label="延误" value="延误" />
-        <el-option label="取消" value="取消" />
-      </el-select>
-    </el-form-item>
-    <!-- 操作按钮 -->
-    <el-form-item>
-      <el-button type="primary" @click="editSubmit()" v-if="form.id">修改</el-button>
-      <el-button type="primary" @click="addSubmit()" v-else>新增</el-button>
-      <el-button @click="btnCancel">重置</el-button>
-    </el-form-item>
-  </el-form>
+  <div>
+    <!-- 商品列表 -->
+    <el-table :data="goodsList" style="width: 100%; margin-bottom: 20px">
+      <el-table-column prop="id" label="航班ID" />
+      <el-table-column prop="flightNumber" label="航班号" sortable />
+      <el-table-column prop="status" label="航班状态" sortable />
+      <el-table-column prop="departureAirport" label="出发机场" />
+      <el-table-column prop="arrivalAirport" label="到达机场" />
+      <el-table-column prop="departureTime" label="出发时间" />
+      <el-table-column prop="arrivalTime" label="到达时间" />
+      <el-table-column prop="createdTime" label="创建时间" />
+      <el-table-column prop="updatedTime" label="更新时间" />
+      <el-table-column fixed="right" label="操作" width="200">
+        <template #default="{ row }">
+          <el-button type="warning" @click="editRow(row)">编辑</el-button>
+          <el-button type="danger" @click="delRow(row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <!-- 表单部分 -->
+    <el-form ref="flightFormRef" :model="form" label-width="120px">
+      <!-- 航班号 -->
+      <el-form-item label="航班号" prop="flightNumber" style="width: 92%">
+        <el-input v-model="form.flightNumber" placeholder="请填写航班号" />
+      </el-form-item>
+      <!-- 出发机场 -->
+      <el-form-item label="出发机场" prop="departureAirport" style="width: 92%">
+        <el-input v-model="form.departureAirport" placeholder="请填写出发机场" />
+      </el-form-item>
+      <!-- 到达机场 -->
+      <el-form-item label="到达机场" prop="arrivalAirport" style="width: 92%">
+        <el-input v-model="form.arrivalAirport" placeholder="请填写到达机场" />
+      </el-form-item>
+      <!-- 出发时间 -->
+      <el-form-item label="出发时间" prop="departureTime" style="width: 92%">
+        <el-date-picker
+          v-model="form.departureTime"
+          type="datetime"
+          placeholder="请选择出发时间"
+          format="YYYY-MM-DD HH:mm:ss"
+          value-format="YYYY-MM-DD HH:mm:ss"
+        />
+      </el-form-item>
+      <!-- 到达时间 -->
+      <el-form-item label="到达时间" prop="arrivalTime" style="width: 92%">
+        <el-date-picker
+          v-model="form.arrivalTime"
+          type="datetime"
+          placeholder="请选择到达时间"
+          format="YYYY-MM-DD HH:mm:ss"
+          value-format="YYYY-MM-DD HH:mm:ss"
+        />
+      </el-form-item>
+      <!-- 航班状态 -->
+      <el-form-item label="航班状态" prop="status" style="width: 92%">
+        <el-select v-model="form.status" placeholder="请选择航班状态">
+          <el-option label="按时" value="按时" />
+          <el-option label="延误" value="延误" />
+          <el-option label="取消" value="取消" />
+        </el-select>
+      </el-form-item>
+      <!-- 操作按钮 -->
+      <el-form-item>
+        <el-button type="primary" @click="editSubmit()" v-if="form.id">修改</el-button>
+        <el-button type="primary" @click="addSubmit()" v-else>新增</el-button>
+        <el-button @click="btnCancel">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script setup>
 import { reactive, ref, onMounted } from 'vue'
 import { getFlightsList, uploadPictureURL, getGoods, addGoods, editGoods } from '../api'
 import useToken from '../stores/token'
-import { Plus } from '@element-plus/icons-vue'
 
 const props = defineProps({
   id: {
@@ -64,7 +85,7 @@ const props = defineProps({
 const emit = defineEmits(['success'])
 
 const form = reactive({
-  id: props.id,
+  id: null,
   flightNumber: '',
   departureAirport: '',
   arrivalAirport: '',
@@ -73,51 +94,36 @@ const form = reactive({
   status: ''
 })
 const flightFormRef = ref()
-const flightsList = ref([])
+const goodsList = ref([])
 
 const { token } = useToken()
 const headers = { jwt: token }
 
 onMounted(() => {
-  loadFlight()
   loadFlights()
 })
 
-const resetForm = id => {
-  form.id = id
-  btnCancel()
+const resetForm = () => {
+  flightFormRef.value.resetFields()
+  form.id = null
+  form.flightNumber = ''
+  form.departureAirport = ''
+  form.arrivalAirport = ''
+  form.departureTime = ''
+  form.arrivalTime = ''
+  form.status = ''
 }
 
 defineExpose({ resetForm })
 
-const loadFlight = async () => {
-  if (form.id) {
-    const goods = await getGoods({ id: form.id })
-    Object.assign(form, goods)
-  }
-}
-
 const loadFlights = async () => {
   const data = await getFlightsList()
-  flightsList.value = convertToTree(data)
+  goodsList.value = data
 }
 
-const convertToTree = data => {
-  const treeData = []
-  const map = {}
-  for (const item of data) {
-    map[item.id] = { ...item, children: [] }
-  }
-  for (const item of data) {
-    const node = map[item.id]
-    if (item.pid === 0) {
-      treeData.push(node)
-    } else {
-      const parent = map[item.pid]
-      parent.children.push(node)
-    }
-  }
-  return treeData
+// 编辑行
+const editRow = (row) => {
+  Object.assign(form, row)
 }
 
 // 新增航班
@@ -130,27 +136,25 @@ const addSubmit = async () => {
     arrivalTime: form.arrivalTime,
     status: form.status
   }
-  if (await addGoods(data)) { // 保持现有API调用不变
+  if (await addGoods(data)) {
     emit('success')
+    resetForm()
+    loadFlights()
   }
 }
 
 // 修改航班
 const editSubmit = async () => {
-  if (await editGoods(form)) { // 保持现有API调用不变
+  if (await editGoods(form)) {
     emit('success')
+    resetForm()
+    loadFlights()
   }
 }
 
 // 重置表单
 const btnCancel = () => {
-  flightFormRef.value.resetFields()
-  form.flightNumber = ''
-  form.departureAirport = ''
-  form.arrivalAirport = ''
-  form.departureTime = ''
-  form.arrivalTime = ''
-  form.status = ''
+  resetForm()
 }
 </script>
 
@@ -163,82 +167,3 @@ const btnCancel = () => {
   width: 91%;
 }
 </style>
-
-
-
-
-
-
-
-
-<!-- 
-<script setup>
-import { reactive, ref, onMounted } from 'vue'
-import { getGoods, addGoods, editGoods } from '../api' // 保持现有API路径不变
-import useToken from '../stores/token'
-
-const props = defineProps({
-  id: {
-    type: Number
-  }
-})
-
-const formRef = ref()
-
-onMounted(() => {
-  loadFlight()
-})
-
-const resetForm = id => {
-  form.id = id
-  btnCancel()
-}
-
-defineExpose({ resetForm })
-
-const loadFlight = async () => {
-  if (form.id) {
-    const flight = await getGoods({ id: form.id }) // 保持现有API调用不变
-    Object.assign(form, flight)
-  }
-}
-
-// 新增航班
-const addSubmit = async () => {
-  const data = {
-    flightNumber: form.flightNumber,
-    departureAirport: form.departureAirport,
-    arrivalAirport: form.arrivalAirport,
-    departureTime: form.departureTime,
-    arrivalTime: form.arrivalTime,
-    status: form.status
-  }
-  if (await addGoods(data)) { // 保持现有API调用不变
-    emit('success')
-  }
-}
-
-// 修改航班
-const editSubmit = async () => {
-  if (await editGoods(form)) { // 保持现有API调用不变
-    emit('success')
-  }
-}
-
-// 重置表单
-const btnCancel = () => {
-  formRef.value.resetFields()
-  form.flightNumber = ''
-  form.departureAirport = ''
-  form.arrivalAirport = ''
-  form.departureTime = ''
-  form.arrivalTime = ''
-  form.status = ''
-}
-</script>
-
-<style scoped>
-.el-form-item {
-  text-align: left;
-}
-</style> -->
